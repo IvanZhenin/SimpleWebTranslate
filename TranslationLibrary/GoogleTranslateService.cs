a7f535e910a4e689328e227875f9ceae193ea014
Ivan Zhenin <ofdof44488kkofsdkofs@mail.ru>
2024-11-07 21:44:33 +0500

    Add first solution (create translator class)

---
+++ TranslationLibrary/GoogleTranslateService.cs
@@ -0,0 +1,31 @@
+﻿using System.Web;
+
+namespace TranslationLibrary
+{
+	public class GoogleTranslateService : ITranslateService
+	{
+		public async Task<string> TranslateTextAsync(string text, string fromLanguage, string toLanguage)
+		{
+			if (String.IsNullOrEmpty(text))
+			{
+				return string.Empty;
+			}
+
+			var url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl={fromLanguage}&tl={toLanguage}&dt=t&q={HttpUtility.UrlEncode(text)}";
+
+			using (var httpClient = new HttpClient())
+			{
+				try
+				{
+					var result = await httpClient.GetStringAsync(url);
+					result = result.Substring(4, result.IndexOf("\"", 4, StringComparison.Ordinal) - 4);
+					return result;
+				}
+				catch
+				{
+					return "*Ошибка*";
+				}
+			}
+		}
+	}
+}
---
+++ TranslationLibrary/ITranslateService.cs
@@ -0,0 +1,7 @@
+﻿namespace TranslationLibrary
+{
+	public interface ITranslateService
+	{
+		public Task<string> TranslateTextAsync(string text, string fromLanguage, string toLanguage);
+	}
+}
